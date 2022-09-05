d9757104e21de1f254ca4964dd4da6ab90cd5109
[Redacted Due to Privacy Issues] <112815870+UNSC-Projects@users.noreply.github.com>
2022-09-05 21:23:59 +0100

    Converter.py v2
    This has added the ability to convert binary to basic text, as in only A-Z (uppercase only) which has been hard codded to automatically switch to uppercase even if not uppercase

--- converter.py
+++ converter.py
@@ -6,6 +6,7 @@ import uuid
 
 
 binary = ""
+word = ""
 letters = []
 
 
@@ -16,7 +17,7 @@ def __init__():
 
 def text_to_binary():
     global binary
-    word = input("Enter a base plate of letters (can only be one word): ")
+    word = input("Enter a sentence / word: ")
     len_word = len(word)
     for x in range(len_word):
         letters.append(word.upper()[x])
@@ -81,3 +82,79 @@ def text_to_binary():
 
     print(f"The Given Word: {word}\tThe Binary Version,")
     print(binary)
+    print("Writing to Binary.txt")
+    with open("Binary.txt", "w") as file:
+        file.write(binary)
+
+def binary_to_text():
+    global word
+    with open("Binary.txt", "r") as file:
+        all_binary = file.read().splitlines()
+    len_binary = len(all_binary)
+
+    for y in range(len_binary):
+        if all_binary[y] == "01000001":
+            word += "A"
+        if all_binary[y] == "01000010":
+            word += "B"
+        if all_binary[y] == "01000011":
+            word += "C"
+        if all_binary[y] == "01000100":
+            word += "D"
+        if all_binary[y] == "01000101":
+            word += "E"
+        if all_binary[y] == "01000110":
+            word += "F"
+        if all_binary[y] == "01000111":
+            word += "G"
+        if all_binary[y] == "01001000":
+            word += "H"
+        if all_binary[y] == "01001001":
+            word += "I"
+        if all_binary[y] == "01001010":
+            word += "J"
+        if all_binary[y] == "01001011":
+            word += "K"
+        if all_binary[y] == "01001100":
+            word += "L"
+        if all_binary[y] == "01001101":
+            word += "M"
+        if all_binary[y] == "01001110":
+            word += "N"
+        if all_binary[y] == "01001111":
+            word += "O"
+        if all_binary[y] == "01010000":
+            word += "P"
+        if all_binary[y] == "01010001":
+            word += "Q"
+        if all_binary[y] == "01010010":
+            word += "R"
+        if all_binary[y] == "01010011":
+            word += "S"
+        if all_binary[y] == "01010100":
+            word += "T"
+        if all_binary[y] == "01010101":
+            word += "U"
+        if all_binary[y] == "01010110":
+            word += "V"
+        if all_binary[y] == "01010111":
+            word += "W"
+        if all_binary[y] == "01011000":
+            word += "X"
+        if all_binary[y] == "01011001":
+            word += "Y"
+        if all_binary[y] == "01011010":
+            word += "Z"
+        if all_binary[y] == "00100000":
+            word += " "
+
+    print("Loading Binary.txt")
+    print(f"The Word is: {word}")
+
+trr = 2
+
+if trr == 1:
+    text_to_binary()
+
+if trr == 2:
+    binary_to_text()
